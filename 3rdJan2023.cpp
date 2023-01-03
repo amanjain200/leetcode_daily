741e995a7141aeedb1d5aa8a8fcdd9c29ce572d2
amanjain200 <amanjain189200@gmail.com>
2023-01-03 11:37:19 +0530

    day3

---
+++ 3rdJan2023.cpp
@@ -0,0 +1,55 @@
+/*
+944. Delete Columns to Make Sorted
+Easy
+781
+2.4K
+Companies
+You are given an array of n strings strs, all of the same length.
+
+The strings can be arranged such that there is one on each line, making a grid. For example, strs = ["abc", "bce", "cae"] can be arranged as:
+
+abc
+bce
+cae
+You want to delete the columns that are not sorted lexicographically. In the above example (0-indexed), columns 0 ('a', 'b', 'c') and 2 ('c', 'e', 'e') are sorted while column 1 ('b', 'c', 'a') is not, so you would delete column 1.
+
+Return the number of columns that you will delete.
+
+ 
+
+Example 1:
+
+Input: strs = ["cba","daf","ghi"]
+Output: 1
+Explanation: The grid looks as follows:
+  cba
+  daf
+  ghi
+Columns 0 and 2 are sorted, but column 1 is not, so you only need to delete 1 column.
+
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int count = 0;
+
+        int len = strs[0].length();
+
+        for(int i=0; i<len; i++){
+            for(int j=0; j<strs.size()-1; j++){
+                string st1 = strs[j];
+                string st2 = strs[j+1];
+                if(st2[i] < st1[i]){
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+
+    }
+};
