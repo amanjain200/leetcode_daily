88fe85d462a65a2a578b06bcef1c86f286a05bb7
amanjain200 <amanjain189200@gmail.com>
2023-01-13 20:57:22 +0530

    day13

---
+++ 13thJan2023.cpp
@@ -0,0 +1,78 @@
+/*
+2246. Longest Path With Different Adjacent Characters
+Hard
+
+You are given a tree (i.e. a connected, undirected graph that has no cycles) rooted at node 0 consisting of n nodes numbered from 0 to n - 1. The tree is represented by a 0-indexed array parent of size n, where parent[i] is the parent of node i. Since node 0 is the root, parent[0] == -1.
+
+You are also given a string s of length n, where s[i] is the character assigned to node i.
+
+Return the length of the longest path in the tree such that no pair of adjacent nodes on the path have the same character assigned to them.
+
+ 
+
+Example 1:
+
+
+Input: parent = [-1,0,0,1,1,2], s = "abacbe"
+Output: 3
+Explanation: The longest path where each two adjacent nodes have different characters in the tree is the path: 0 -> 1 -> 3. The length of this path is 3, so 3 is returned.
+It can be proven that there is no longer path that satisfies the conditions. 
+Example 2:
+
+
+Input: parent = [-1,0,0,0], s = "aabc"
+Output: 3
+Explanation: The longest path where each two adjacent nodes have different characters is the path: 2 -> 0 -> 3. The length of this path is 3, so 3 is returned.
+ 
+
+Constraints:
+
+n == parent.length == s.length
+1 <= n <= 105
+0 <= parent[i] <= n - 1 for all i >= 1
+parent[0] == -1
+parent represents a valid tree.
+s consists of only lowercase English letters.
+
+
+*/
+
+
+/*
+class Solution {
+public:
+    int longestPathBelow(const vector<vector<int>>& graph, int u,
+                          const string& s, int& ans) {
+    int maxi1 = 0, maxi2 = 0;
+
+    for (const int v : graph[u]) {
+      const int result = longestPathBelow(graph, v, s, ans);
+      if (s[u] == s[v])
+        continue;
+      if (result > maxi1) {
+        maxi2 = maxi1;
+        maxi1 = result;
+      } else if (result > maxi2) {
+        maxi2 = result;
+      }
+    }
+
+    ans = max(ans, 1 + maxi1 + maxi2);
+    return 1 + maxi1;
+  }
+
+
+
+    int longestPath(vector<int>& parent, string s) {
+        const int siz = parent.size();
+    int ans = 1;
+    vector<vector<int>> graph(siz);
+
+    for (int i = 1; i < siz; ++i)
+      graph[parent[i]].push_back(i);
+
+    longestPathBelow(graph, 0, s, ans);
+    return ans;
+    }
+};
+*/
