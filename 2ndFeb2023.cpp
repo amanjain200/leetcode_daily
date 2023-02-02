378f189fd8783bb5bdc61a05b994532b31691f96
amanjain200 <amanjain189200@gmail.com>
2023-02-02 20:09:57 +0530

    day33

---
+++ 2ndFeb2023.cpp
@@ -0,0 +1,64 @@
+/*
+953. Verifying an Alien Dictionary
+Easy
+4.1K
+1.3K
+Companies
+In an alien language, surprisingly, they also use English lowercase letters, but possibly in a different order. The order of the alphabet is some permutation of lowercase letters.
+
+Given a sequence of words written in the alien language, and the order of the alphabet, return true if and only if the given words are sorted lexicographically in this alien language.
+
+ 
+
+Example 1:
+
+Input: words = ["hello","leetcode"], order = "hlabcdefgijkmnopqrstuvwxyz"
+Output: true
+Explanation: As 'h' comes before 'l' in this language, then the sequence is sorted.
+Example 2:
+
+Input: words = ["word","world","row"], order = "worldabcefghijkmnpqstuvxyz"
+Output: false
+Explanation: As 'd' comes after 'l' in this language, then words[0] > words[1], hence the sequence is unsorted.
+Example 3:
+
+Input: words = ["apple","app"], order = "abcdefghijklmnopqrstuvwxyz"
+Output: false
+Explanation: The first three characters "app" match, and the second string is shorter (in size.) According to lexicographical rules "apple" > "app", because 'l' > '∅', where '∅' is defined as the blank character which is less than any other character (More info).
+ 
+
+Constraints:
+
+1 <= words.length <= 100
+1 <= words[i].length <= 20
+order.length == 26
+All characters in words[i] and order are English lowercase letters.
+
+*/
+
+#include<iostream>
+#include<unordered_map>
+#include<vector>
+#include<string>
+using namespace std;
+
+
+
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char,char> mp;
+        
+        for(int i=0;i<26;i++){
+            mp[order[i]]= ('a'+i);
+        }
+        
+        for(int i=0;i<words.size();i++){
+            for(int j=0;j<words[i].size();j++){
+                words[i][j]= mp[words[i][j]];
+            }
+        }
+        
+        //return is_sorted(words.begin(),words.end());
+    }
+};
