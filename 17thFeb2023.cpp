cb5120c197d5742f39831d5068f8be74681b81ea
amanjain200 <amanjain189200@gmail.com>
2023-02-17 17:26:31 +0530

    day48

---
+++ 17thFeb2023.cpp
@@ -0,0 +1,69 @@
+/*
+Is it Fibonacci
+EasyAccuracy: 53.86%Submissions: 11K+Points: 2
+Lamp
+You can earn more Geekbits by participating in GFG Weekly Coding Contest  
+
+Geek just learned about Fibonacci numbers.
+
+The Fibonacci Sequence is the series of numbers: 0, 1, 1, 2, 3, 5, 8, 13, ...
+where the next number is found by adding up the two numbers before it.
+
+He defines a new series called Geeky numbers. Here the next number is the sum of the K preceding numbers.
+You are given an array of size K, GeekNum[ ], where the ith element of the array represents the ith Geeky number. Return its Nth term.
+
+Note: This problem can be solved in O(N2) time complexity but the user has to solve this in O(N). The Constraints are less because there can be integer overflow in the terms.
+
+Example 1:
+
+Input:
+N = 6, K = 1
+GeekNum[] = {4}
+Output: 
+4
+Explanation: 
+Terms are 4, 4, 4, 4, 4, 4
+Example 2:
+
+Input:
+N = 5, K = 3
+GeekNum[] = {0, 1, 2}
+Output: 
+6
+Explanation: 
+Terms are 0, 1, 2, 3, 6.
+So the 5th term is 6
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function solve( ) which takes integer N, K, and an array GeekNum[] as input parameters and returns the Nth term of the Geeky series.
+
+Expected Time Complexity: O(N)
+Expected Space Complexity: O(N)
+
+Constraints:
+1 ≤ K < 30
+1 ≤ N ≤ 70
+K ≤ N
+0 < GeekNum[ ] < 100
+*/
+#include<iostream>
+#include<vector>
+using namespace std;
+
+// User function Template for C++
+
+class Solution {
+  public:
+    long long solve(int N, int K, vector<long long> GeekNum) {
+        if(K==1) return GeekNum[0];
+        long long sum=0;
+        for(int i=0; i<K; i++){
+            sum+=GeekNum[i];
+        }
+        GeekNum.push_back(sum);
+        for(int i=K+1; i<N; i++){
+            sum+=GeekNum[i-1] - GeekNum[i-K-1];
+            GeekNum.push_back(sum);
+        }
+        return GeekNum[N-1];
+    }
+};
