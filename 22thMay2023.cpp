0e016df9f930b565433cf00eb503dbbc257233bc
amanjain200 <amanjain189200@gmail.com>
2023-05-22 11:51:09 +0530

    day142

---
+++ 22thMay2023.cpp
@@ -0,0 +1,61 @@
+/*
+347. Top K Frequent Elements
+Medium
+13.8K
+498
+Companies
+Given an integer array nums and an integer k, return the k most frequent elements. You may return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,1,1,2,2,3], k = 2
+Output: [1,2]
+Example 2:
+
+Input: nums = [1], k = 1
+Output: [1]
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+-104 <= nums[i] <= 104
+k is in the range [1, the number of unique elements in the array].
+It is guaranteed that the answer is unique.
+ 
+
+Follow up: Your algorithm's time complexity must be better than O(n log n), where n is the array's size.
+*/
+
+/*
+class Solution {
+    /*static bool cmp(int p, int q){
+        return q >= p;
+    }
+    /*struct cmp{
+        bool operator()(int a, int b) const {return a<b;}
+    };
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> mp;
+        for(auto it : nums){
+            mp[it]++;
+        }
+        //sort(mp.begin(), mp.end(), cmp);
+        set<pair<int,int>> st;
+        for(auto it : mp){
+            st.insert({it.second, it.first});
+        }
+        vector<int> ans;
+        set<pair<int,int>> :: iterator it = st.end();
+        it--;
+        while(k--){
+            ans.push_back(it->second);
+            it--;
+        }
+        return ans;
+    }
+};
+*/
