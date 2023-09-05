968e520cb29a46d88f85262a8c7505c9ca481b29
amanjain200 <amanjain189200@gmail.com>
2023-09-05 21:43:20 +0530

    Day248

---
+++ 5thSept2023.cpp
@@ -0,0 +1,111 @@
+/*
+138. Copy List with Random Pointer
+Medium
+12.6K
+1.3K
+Companies
+
+A linked list of length n is given such that each node contains an additional random pointer, which could point to any node in the list, or null.
+
+Construct a deep copy of the list. The deep copy should consist of exactly n brand new nodes, where each new node has its value set to the value of its corresponding original node. Both the next and random pointer of the new nodes should point to new nodes in the copied list such that the pointers in the original list and copied list represent the same list state. None of the pointers in the new list should point to nodes in the original list.
+
+For example, if there are two nodes X and Y in the original list, where X.random --> Y, then for the corresponding two nodes x and y in the copied list, x.random --> y.
+
+Return the head of the copied linked list.
+
+The linked list is represented in the input/output as a list of n nodes. Each node is represented as a pair of [val, random_index] where:
+
+    val: an integer representing Node.val
+    random_index: the index of the node (range from 0 to n-1) that the random pointer points to, or null if it does not point to any node.
+
+Your code will only be given the head of the original linked list.
+
+ 
+
+Example 1:
+
+Input: head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+Output: [[7,null],[13,0],[11,4],[10,2],[1,0]]
+
+Example 2:
+
+Input: head = [[1,1],[2,1]]
+Output: [[1,1],[2,1]]
+
+Example 3:
+
+Input: head = [[3,null],[3,0],[3,null]]
+Output: [[3,null],[3,0],[3,null]]
+
+ 
+
+Constraints:
+
+    0 <= n <= 1000
+    -104 <= Node.val <= 104
+    Node.random is null or is pointing to some node in the linked list.
+
+
+
+*/
+/*
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        int no = 1;
+        map<Node*, pair<int, Node*>> original;
+        Node* temp = head;
+        while(temp != NULL){
+            original[temp] = {no, temp->random};
+            no++;
+            temp=temp->next;
+        }
+        //dummy node for deepcopy
+        Node* ans = new Node(0);
+        Node* ansHead = ans;
+
+        map<int, Node*> deepcopy;
+        no=1;
+        for(auto it : original){
+            Node* org = it.first;
+            Node* ansNode = new Node(it.first->val);
+            ans->next = ansNode;
+            ans=ans->next;
+            deepcopy[no]=ans;
+            no++;
+        }
+
+        for(auto it : original){
+            Node* dcnode = deepcopy[it.second.first];
+            Node* randNode = it.second.second;
+            if(randNode==NULL){
+
+            }else{
+                int randNo = original[randNode].first;
+                dcnode->random = deepcopy[randNo];
+            }
+
+        }
+
+        return ansHead->next;
+    }
+};
+
+
+*/
